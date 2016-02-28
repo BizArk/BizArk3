992ea5de9b7b873eb4f80d0950ee2dc8700ca864
bbrewder <github@brewder.com>
2016-02-28 12:25:53 -0800

    Moved the sample console class to it's own file and added a new basic console sample (no attributes).

---
+++ BizArk.ConsoleApp.Sample/BasicConsoleApp.cs
@@ -0,0 +1,37 @@
+﻿using BizArk.Core.Extensions.FormatExt;
+using System;
+
+namespace BizArk.ConsoleApp.Sample
+{
+
+	/// <summary>
+	/// This class demonstrates a very basic console application without using any attributes.
+	/// </summary>
+	public class BasicConsoleApp : BaseConsoleApp
+	{
+
+		public string Name { get; set; }
+
+		public DateTime BirthDate { get; set; }
+
+		public override int Start()
+		{
+			var today = DateTime.Today;
+			if (BirthDate.Month == today.Month && BirthDate.Day == today.Day)
+			{
+				BaCon.WriteLine("Happy birthday {0}!!!".Fmt(Name), ConsoleColor.Red);
+			}
+			else
+			{
+				var next = BirthDate.AddYears(today.Year - BirthDate.Year);
+				if (next < today)
+					next = next.AddYears(1);
+				int numDays = (next - today).Days;
+				BaCon.WriteLine("Only {0} days until your birthday!".Fmt(numDays));
+			}
+
+			return 0; // 0 for success.
+		}
+
+	}
+}
--- BizArk.ConsoleApp.Sample/Program.cs
+++ BizArk.ConsoleApp.Sample/Program.cs
@@ -1,72 +1,12 @@
-﻿using BizArk.Core.Extensions.FormatExt;
-using System;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-
-namespace BizArk.ConsoleApp.Sample
+﻿namespace BizArk.ConsoleApp.Sample
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			BaCon.Start<SampleConsoleApp>();
+			//BaCon.Start<BasicConsoleApp>();
 		}
 	}
 
-	[CmdLineOptions("Name")]
-	public class SampleConsoleApp : BaseConsoleApp
-	{
-
-		public SampleConsoleApp()
-		{
-			// Setup the default values.
-			HasHair = true;
-			Status = MaritalStatus.Unknown;
-		}
-
-		[Required]
-		[StringLength(20, MinimumLength = 2)]
-		[Description("The name of the person.")]
-		public string Name { get; set; }
-
-		[Required]
-		[Range(0, 120)]
-		[Description("The age in years of the person.")]
-		public int Age { get; set; }
-
-		[CmdLineArg("Job", ShowInUsage = Core.DefaultBoolean.True)]
-		[Description("The type of job the person has.")]
-		public string Occupation { get; set; }
-
-		[CmdLineArg(ShowInUsage = Core.DefaultBoolean.True)]
-		[Description("Does the person have hair.")]
-		public bool HasHair { get; set; }
-
-		[Description("Names of the person's children.")]
-		public string[] Children { get; set; }
-
-		[CmdLineArg(ShowInUsage = Core.DefaultBoolean.True)]
-		[Description("Marital status of the person.")]
-		public MaritalStatus Status { get; set; }
-
-		public override int Start()
-		{
-			BaCon.WriteLine("Name={0}".Fmt(Name), ConsoleColor.White);
-			BaCon.WriteLine("Age={0}".Fmt(Age));
-			BaCon.WriteLine("Job={0}".Fmt(Occupation));
-			BaCon.WriteLine("HasHair={0}".Fmt(HasHair));
-			BaCon.WriteLine("Children={0}".Fmt(string.Join(", ", Children)));
-			BaCon.WriteLine("Marital Status={0}".Fmt(Status));
-
-			return 0;
-		}
-
-		public enum MaritalStatus
-		{
-			Single,
-			Married,
-			ItsComplicated,
-			Unknown
-		}
-	}
 }
