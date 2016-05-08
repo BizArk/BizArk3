5b816104b744b5fd86da6e1d07004eef7e291a4d
bbrewder <github@brewder.com>
2016-05-08 12:49:13 -0700

    Adding SelectCmdBuilder and supporting classes and tests.

---
+++ BizArk.Data.SqlServer/SqlParameterList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BizArk.Core.Extensions.ObjectExt;
+
+namespace BizArk.Data.SqlServer
+{
+
+	/// <summary>
+	/// Provides some useful functions for a list of SqlParameters.
+	/// </summary>
+	public class SqlParameterList : List<SqlParameter>
+	{
+
+		/// <summary>
+		/// Add a SqlParameter to the list.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public SqlParameter AddWithValue(string name, object value)
+		{
+			var param = new SqlParameter(name, value);
+			this.Add(param);
+			return param;
+		}
+
+		/// <summary>
+		/// Adds the properties of the object as parameters. Uses BizArk.Core.Util.ObjectUtil.ToPropertyBag to get values.
+		/// </summary>
+		/// <param name="values"></param>
+		/// <returns></returns>
+		public int AddValues(object values)
+		{
+			var propBag = values.ToPropertyBag();
+			var count = 0;
+			foreach(var prop in propBag)
+			{
+				AddWithValue(prop.Key, prop.Value);
+				count++;
+			}
+			return count;
+		}
+
+	}
+
+}
