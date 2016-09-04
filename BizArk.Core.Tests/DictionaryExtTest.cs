a5ea52cee37efe3e5d998d85281a46c6dd1caa3d
brian <brian@brewder.com>
2016-09-03 17:12:49 -0700

    Added some extension methods to make it easier to convert values in dictionaries.

---
+++ BizArk.Core.Tests/DictionaryExtTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BizArk.Core.Extensions.DictionaryExt;
+
+namespace BizArk.Core.Tests
+{
+    [TestFixture]
+    public class DictionaryExtTest
+    {
+        [Test]
+        public void ConvertValueFromPropertyBag()
+        {
+            var dict = new Dictionary<string, object>();
+            var val = dict.GetValue<int>("Test");
+            Assert.AreEqual(0, val);
+
+            dict["Test"] = "123";
+            val = dict.GetValue<int>("Test");
+            Assert.AreEqual(123, val);
+
+            val = dict.GetValue<int>("INVALID", -1);
+            Assert.AreEqual(-1, val);
+        }
+
+        [Test]
+        public void ConvertValueFromIntStringDict()
+        {
+            var dict = new Dictionary<int, string>();
+            var val = dict.GetValue<int, string, decimal>(123);
+            Assert.AreEqual(0m, val);
+
+            dict[123] = "1.23";
+            val = dict.GetValue<int, string, decimal>(123);
+            Assert.AreEqual(1.23m, val);
+
+            val = dict.GetValue<int, string, decimal>(-1, decimal.MinValue);
+            Assert.AreEqual(decimal.MinValue, val);
+        }
+    }
+}
---
+++ BizArk.Core/Extensions/DictionaryExt.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizArk.Core.Extensions.DictionaryExt
+{
+
+    /// <summary>
+    /// Provides extension methods to make it easier to work with dictionaries.
+    /// </summary>
+    public static class DictionaryExt
+    {
+
+        #region Convert
+
+        /// <summary>
+        /// Converts the dictionary into a dynamic object (ExpandoObject).
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        public static dynamic ToDynamic<TVal>(this IDictionary<string, TVal> dict)
+        {
+            dynamic obj = new ExpandoObject();
+            foreach(var item in dict)
+            {
+                obj[item.Key] = item.Value;
+            }
+            return obj;
+        }
+
+        /// <summary>
+        /// Gets the specified value from the dictionary. Converts to the given type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key">The key to the dictionary.</param>
+        /// <param name="dflt">Returned if the key is not found or if the dictionary is null.</param>
+        /// <returns></returns>
+        public static T GetValue<T>(this IDictionary<string, object> dict, string key, T dflt = default(T))
+        {
+            if (dict == null) return dflt;
+            if (!dict.ContainsKey(key)) return dflt;
+
+            var val = dict[key];
+            return ConvertEx.To<T>(val);
+        }
+
+        /// <summary>
+        /// Gets the specified value from the dictionary. Converts to the given type.
+        /// </summary>
+        /// <typeparam name="TKey">Key type for the dictionary.</typeparam>
+        /// <typeparam name="TVal">Value type for the dictionary.</typeparam>
+        /// <typeparam name="TRet">Type to convert the value to.</typeparam>
+        /// <param name="dict"></param>
+        /// <param name="key">The key to the dictionary.</param>
+        /// <param name="dflt">Returned if the key is not found or if the dictionary is null.</param>
+        /// <returns></returns>
+        public static TRet GetValue<TKey, TVal, TRet>(this IDictionary<TKey, TVal> dict, TKey key, TRet dflt = default(TRet))
+        {
+            if (dict == null) return dflt;
+            if (!dict.ContainsKey(key)) return dflt;
+
+            var val = dict[key];
+            return ConvertEx.To<TRet>(val);
+        }
+
+        #endregion
+
+    }
+}
