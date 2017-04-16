3380391a5ab7c59fc856a170bc8a2f6e9b659c58
brian <brian@brewder.com>
2017-04-15 19:50:28 -0700

    Updated assembly properties.

--- BizArk.ConsoleApp/Properties/AssemblyInfo.cs
+++ BizArk.ConsoleApp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CmdLineConsole")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BizArk Console App")]
+[assembly: AssemblyDescription("Provides utilities for console applications.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CmdLineConsole")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("BizArk")]
+[assembly: AssemblyProduct("BaCon")]
+[assembly: AssemblyCopyright("Copyright © BizArk 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
 
 [assembly: InternalsVisibleTo("BizArk.ConsoleApp.Tests")]
