e5261c6c67fdfbc2f78f0494467b24ecb4159c89
brian <brian@brewder.com>
2018-11-10 20:53:52 -0800

    The BaTableRepository was Sql Server specific. Move to SqlServer folder and change to extension methods.

--- BizArk.Data/SqlServer/SqlServerBaTableExt.cs
+++ BizArk.Data/SqlServer/SqlServerBaTableExt.cs
@@ -4,58 +4,24 @@ using BizArk.Data.SqlServer.CrudExt;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace BizArk.Data.SqlServer
+namespace BizArk.Data.SqlServer.BaTableExt
 {
 
 	/// <summary>
 	/// Provides repository methods for BaTableObjects.
 	/// </summary>
-	public class BaTableRepository<T> : IBaRepository where T : BaTableObject, new()
+	public static class SqlServerBaTableExt
 	{
-
-		#region Initialization and Destruction
-
-		/// <summary>
-		/// Creates a new instance of BaTableRepository.
-		/// </summary>
-		/// <param name="db">The database to use for the repository. The database will not be disposed with the repository.</param>
-		public BaTableRepository(ISupportBaDatabase db)
-		{
-			DB = db.DB;
-			InitTableName();
-		}
-
-		private void InitTableName()
-		{
-			// Instantiate an object just so we can get the table name.
-			var obj = new T();
-			TableName = obj.TableName;
-		}
-
-		#endregion
-
-		#region Fields and Properties
-
-		/// <summary>
-		/// Gets the name of the table in the database.
-		/// </summary>
-		public string TableName { get; private set; }
-
-		/// <summary>
-		/// Gets the database associated with this repository.
-		/// </summary>
-		public BaDatabase DB { get; private set; }
-
-		#endregion
 
 		#region Methods
 
 		/// <summary>
 		/// Saves the table object (INSERT and UPDATE). Does not handle validation.
 		/// </summary>
+		/// <param name="rep"></param>
 		/// <param name="obj"></param>
 		/// <param name="restricted"></param>
-		public virtual void Save(T obj, params string[] restricted)
+		public static void Save<T>(this IBaRepository rep, T obj, params string[] restricted) where T : BaTableObject
 		{
 			if (obj == null) return;
 
@@ -65,7 +31,7 @@ namespace BizArk.Data.SqlServer
 
 			if (obj.IsNew)
 			{
-				var values = DB.Insert(obj.TableName, updates);
+				var values = rep.DB.Insert(obj.TableName, updates);
 				obj.Fill((object)values);
 			}
 			else
@@ -73,7 +39,7 @@ namespace BizArk.Data.SqlServer
 				var key = new Dictionary<string, object>();
 				foreach (var fld in obj.GetKey())
 					key.Add(fld.Name, fld.Value);
-				DB.Update(obj.TableName, key, updates);
+				rep.DB.Update(obj.TableName, key, updates);
 			}
 			obj.UpdateDefaults();
 		}
@@ -81,9 +47,10 @@ namespace BizArk.Data.SqlServer
 		/// <summary>
 		/// Saves the table object (INSERT and UPDATE). Does not handle validation.
 		/// </summary>
+		/// <param name="rep"></param>
 		/// <param name="obj"></param>
 		/// <param name="restricted"></param>
-		public async virtual Task SaveAsync(T obj, params string[] restricted)
+		public static async Task SaveAsync<T>(this IBaRepository rep, T obj, params string[] restricted) where T : BaTableObject, new()
 		{
 			if (obj == null) return;
 
@@ -93,7 +60,7 @@ namespace BizArk.Data.SqlServer
 
 			if (obj.IsNew)
 			{
-				var values = await DB.InsertAsync(obj.TableName, updates).ConfigureAwait(false);
+				var values = await rep.DB.InsertAsync(obj.TableName, updates).ConfigureAwait(false);
 				obj.Fill((object)values);
 			}
 			else
@@ -101,7 +68,7 @@ namespace BizArk.Data.SqlServer
 				var key = new Dictionary<string, object>();
 				foreach (var fld in obj.GetKey())
 					key.Add(fld.Name, fld.Value);
-				await DB.UpdateAsync(obj.TableName, key, updates).ConfigureAwait(false);
+				await rep.DB.UpdateAsync(obj.TableName, key, updates).ConfigureAwait(false);
 			}
 			obj.UpdateDefaults();
 		}
@@ -109,8 +76,9 @@ namespace BizArk.Data.SqlServer
 		/// <summary>
 		/// Deletes the table object from the database.
 		/// </summary>
+		/// <param name="rep"></param>
 		/// <param name="obj"></param>
-		public virtual void Delete(T obj)
+		public static void Delete<T>(this IBaRepository rep, T obj) where T : BaTableObject
 		{
 			// Need to convert the key from a list of BaField to 
 			// something that can be converted to a property bag.
@@ -119,14 +87,15 @@ namespace BizArk.Data.SqlServer
 			foreach (var keyFld in keyFlds)
 				key.Add(keyFld.Name, keyFld.Value);
 
-			DB.Delete(obj.TableName, key);
+			rep.DB.Delete(obj.TableName, key);
 		}
 
 		/// <summary>
 		/// Deletes the table object from the database.
 		/// </summary>
+		/// <param name="rep"></param>
 		/// <param name="obj"></param>
-		public async virtual Task DeleteAsync(T obj)
+		public static async Task DeleteAsync<T>(this IBaRepository rep, T obj) where T : BaTableObject
 		{
 			// Need to convert the key from a list of BaField to 
 			// something that can be converted to a property bag.
@@ -135,19 +104,20 @@ namespace BizArk.Data.SqlServer
 			foreach (var keyFld in keyFlds)
 				key.Add(keyFld.Name, keyFld.Value);
 
-			await DB.DeleteAsync(obj.TableName, key).ConfigureAwait(false);
+			await rep.DB.DeleteAsync(obj.TableName, key).ConfigureAwait(false);
 		}
 
 		/// <summary>
 		/// Gets the requested object out of the database.
 		/// </summary>
+		/// <param name="rep"></param>
 		/// <param name="key">The key to </param>
 		/// <param name="flds">List of fields to get out of the database. Leave empty to retrieve all. Names are case sensitive.</param>
 		/// <returns></returns>
-		public virtual IEnumerable<T> Get(object key, params string[] flds)
+		public static IEnumerable<T> Get<T>(this IBaRepository rep, object key, params string[] flds) where T : BaTableObject, new()
 		{
 			var obj = new T();
-			var selCmd = new SelectCmdBuilder(TableName);
+			var selCmd = new SelectCmdBuilder(obj.TableName);
 
 			// Add specified fields to the select command.
 			foreach (var fldName in flds)
@@ -165,19 +135,20 @@ namespace BizArk.Data.SqlServer
 			}
 
 			var cmd = selCmd.CreateCmd();
-			return DB.GetObjects<T>(cmd);
+			return rep.DB.GetObjects<T>(cmd);
 		}
 
 		/// <summary>
 		/// Gets the requested object out of the database.
 		/// </summary>
+		/// <param name="rep"></param>
 		/// <param name="key">The key to </param>
 		/// <param name="flds">List of fields to get out of the database. Leave empty to retrieve all. Names are case sensitive.</param>
 		/// <returns></returns>
-		public async virtual Task<IEnumerable<T>> GetAsync(object key, params string[] flds)
+		public static async Task<IEnumerable<T>> GetAsync<T>(this IBaRepository rep, object key, params string[] flds) where T : BaTableObject, new()
 		{
 			var obj = new T();
-			var selCmd = new SelectCmdBuilder(TableName);
+			var selCmd = new SelectCmdBuilder(obj.TableName);
 
 			// Add specified fields to the select command.
 			foreach (var fldName in flds)
@@ -195,7 +166,7 @@ namespace BizArk.Data.SqlServer
 			}
 
 			var cmd = selCmd.CreateCmd();
-			return await DB.GetObjectsAsync<T>(cmd).ConfigureAwait(false);
+			return await rep.DB.GetObjectsAsync<T>(cmd).ConfigureAwait(false);
 		}
 
 		#endregion
